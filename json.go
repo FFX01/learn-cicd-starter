1c8b2d0c898776a9c9186db26d7841f514b32c2e
Justin Walters <walters.justin01@gmail.com>
2025-02-03 12:24:50 -0800

    fix failing test

--- json.go
+++ json.go
@@ -29,7 +29,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
     _, err = w.Write(dat)
     if err != nil {
-        log.Printf("Error writing to socket", err)
+        log.Printf("Error writing to socket")
         w.WriteHeader(500)
         return
     }
